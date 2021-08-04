a64bfc657e5d84396936678559e48eddf3739ccc
hieu1999xl <hiu99xl@gmail.com>
2021-08-05 00:07:58 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import ProductFeature from "./features/Product";
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import "./App.css";
+import NotFound from "./components/NotFound";
+import AlbumFeature from "./features/Album";
+import CounterFeature from "./features/Counter";
+import TodoFeature from "./features/Todo";
+import CartFeature from "./features/Cart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+
+      <Switch>
+        <Redirect from="/home" to="/" exact />
+        <Redirect from="/post-list/:postId" to="/posts/:postId" exact />
+
+        <Route path="/" component={CounterFeature} exact />
+        <Route path="/todos" component={TodoFeature} />
+        <Route path="/albums" component={AlbumFeature} />
+        <Route path="/products" component={ProductFeature} />
+        <Route path="/cart" component={CartFeature} />
+
+        <Route component={NotFound} />
+      </Switch>
     </div>
   );
 }
